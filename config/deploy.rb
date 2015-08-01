08caab41c0faeb92d7ad46e96528f6d64b610c3d
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2015-08-01 17:00:15 +0200

    deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,48 +3,26 @@ require 'mina/rails'
 require 'mina/git'
 require 'mina/rbenv'
 
-set :domain, 'gitac.local'
-set :deploy_to, '/var/www/web'
-set :app_path, lambda { "#{deploy_to}/#{current_path}" }
 set :repository, 'git@github.com:hermanzdosilovic/web.git'
 set :branch, 'master'
+
 set :user, 'pi'
+set :domain, 'gitac.local'
+set :deploy_to, '/var/www/web'
 set :port, '22219'
-set :forward_agent, true     # SSH forward_agent.
+set :forward_agent, true
 
-set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log']
+set :shared_paths, ['log', 'public/system']
 
-task :environment do
-  invoke :'rbenv:load'
-end
-
-task :setup => :environment do
+task setup: :environment do
   queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
-
-  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
-
-  queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
-  queue! %[touch "#{deploy_to}/#{shared_path}/config/secrets.yml"]
-  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml' and 'secrets.yml'."]
-
-  queue %[
-    repo_host=`echo $repo | sed -e 's/.*@//g' -e 's/:.*//g'` &&
-    repo_port=`echo $repo | grep -o ':[0-9]*' | sed -e 's/://g'` &&
-    if [ -z "${repo_port}" ]; then repo_port=22; fi &&
-    ssh-keyscan -p $repo_port -H $repo_host >> ~/.ssh/known_hosts
-  ]
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/system"]
 end
 
 desc "Deploys the current version to the server."
-task :deploy => :environment do
-  to :before_hook do
-    # Put things to run locally before ssh
-  end
+task deploy: :environment do
   deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
@@ -57,4 +35,8 @@ task :deploy => :environment do
       queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
     end
   end
+end
+
+task :environment do
+  invoke :'rbenv:load'
 end
