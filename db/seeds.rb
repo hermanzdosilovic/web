90998be2ae31936d3053a5f59ee26c7c55b2c562
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2015-08-02 22:19:22 +0200

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(
+  first_name: 'Herman Zvonimir',
+  last_name: 'Došilović',
+  username: 'herman',
+  email: 'hermanz.dosilovic@gmail.com',
+  password: 'changemetosomethingelse',
+  password_confirmation: 'changemetosomethingelse'
+);
