d14bc463413f7ee007b5e288b669101f0712b43c
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2015-08-09 01:06:38 +0200

    html.slim to slim

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -1,5 +1,9 @@
 module Admin
   class RegistrationsController < Devise::RegistrationsController
+    def new
+      raise ActionController::RoutingError.new('Not Found')
+    end
+
     def destroy
       if resource.valid_password?(params[:user][:password])
         render js: "window.location = '#{root_path}'"
