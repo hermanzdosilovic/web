90d74abbef6eb5f55a235750f89cfc918e480a07
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2015-08-10 22:30:58 +0200

    Configured mailers

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,8 +12,7 @@ set :deploy_to, '/var/www/web'
 set :port, '22219'
 set :forward_agent, true
 
-set :shared_paths, ['log', 'public/system']
-set :linked_files, %w{.env}
+set :shared_paths, %w{.env}
 
 task setup: :environment do
   queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
