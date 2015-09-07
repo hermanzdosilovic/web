68918aafe6eed194d45a7e79562746bf0e989039
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2015-09-07 22:06:24 +0200

    Major frontend change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,16 +8,16 @@
 //= norequire get-shit-done
 
 $(document).ready(function(){
-  $(function() {
-      $('body').on('click', '.page-scroll a', function(event) {
-          var $anchor = $(this);
-          $('html, body').stop().animate({
-              scrollTop: $($anchor.attr('href')).offset().top
-          }, 1500, 'easeInOutExpo');
-          event.preventDefault();
-      });
+  $('body').on('click', '.page-scroll a', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
   });
-  $('.nav a').on('click', function(){
-    $(".navbar-toggle").click()
+  $('body').on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
   });
 });
