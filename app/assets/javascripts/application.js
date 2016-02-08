dad2dff14bcec562726698fcd21f151db42035cb
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2016-02-08 17:18:06 +0100

    Lets build clean front

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,36 +1,6 @@
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap-sprockets
-//= require modal_controller
+//= require jquery.min
+//= require bootstrap
 //= require weloveiconfonts_rails
-//= require jquery.easing
-//= require clean-blog
 
 $(document).ready(function(){
-  $('body').on('click', '.page-scroll a', function(event) {
-      var $anchor = $(this);
-      $('html, body').stop().animate({
-          scrollTop: $($anchor.attr('href')).offset().top
-      }, 1500, 'easeInOutExpo');
-      event.preventDefault();
-  });
-  $('body').on('click', '.navbar-custom',function(e) {
-    if( $(e.target).is('a') ) {
-        $('.navbar-collapse.in').collapse('hide');
-    }
-  });
-  $('body').scrollspy({
-    target: '.navbar-fixed-top'
-  });
-
-  window.onresize = function () {
-    $('.fullscreen').css('height', $(window).height());
-    $('.min-fullscreen').css('min-height', $(window).height());
-  };
-  $('.fullscreen').css('height', $(window).height());
-  $('.min-fullscreen').css('min-height', $(window).height());
-
-  $('body').on('click', '#mail', function() {
-    $(this).attr('href', 'mailto:hermanz.dosilovic@gmail.com');
-  });
 });
