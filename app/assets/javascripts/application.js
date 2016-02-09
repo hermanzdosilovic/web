6cbb7e1df3bf24cf43da9f0a28433b5da1cfd8ec
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2016-02-09 15:07:05 +0100

    More cssing and jsing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,31 @@
 //= require jquery.min
-//= require jquery.color.min
 //= require bootstrap
 //= require weloveiconfonts_rails
 //= require custom
 
-$(document).ready(function(){
-  navbarChangeColorOnScroll(".navigation .navbar");
-  changeHeightOnResize(".fullscreen", 0);
+$(document).ready(function() {
   changeMinHeightOnResize(".min-fullscreen", 0);
+
+  var $window = $(window);
+  var $navbar = $(".navigation .navbar");
+
+  $("a.scroll").click(function() {
+    $('.navbar-collapse').collapse('hide');
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500);
+    return false;
+  });
+
+  $('body').scrollspy({
+    target: '.navbar-fixed-top'
+  });
+
+  $window.scroll(function(event) {
+    if ($window.scrollTop() > 0) {
+      $navbar.removeClass("transparent")
+    } else {
+      $navbar.addClass("transparent");
+    }
+  });
 });
--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -11,32 +11,3 @@ function changeMinHeightOnResize(selector, navbarHeight) {
   });
   $(selector).css("min-height", $(window).height() - navbarHeight);
 }
-
-function navbarChangeColorOnScroll(selector) {
-  var detached = false;
-  var animationSpeed = 200;
-  var navbarHeight = 62;
-  $(window).scroll(function(event){
-    if ($(window).width() < 768) {
-      return;
-    }
-    var st = $(this).scrollTop();
-    if (st > navbarHeight && detached === false) {
-      $(selector).animate({
-          backgroundColor: "rgba(62, 63, 58, 0.8)",
-          borderColor: "rgba(62, 63, 58, 0.8)"
-        },
-        animationSpeed
-      );
-      detached = true;
-    } else if (st <= navbarHeight) {
-      $(selector).animate({
-          backgroundColor: "transparent",
-          borderColor: "transparent"
-        },
-        animationSpeed
-      );
-      detached = false;
-    }
-  });
-}
